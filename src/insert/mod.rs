82fd88b42d1513f58ec860178164305386768f12
Anton Davydov <fetsorn@gmail.com>
2025-01-16 05:40:24 +0300

    pass all insert tests

--- src/insert/mod.rs
+++ src/insert/mod.rs
@@ -2,11 +2,10 @@ use crate::types::entry::Entry;
 use crate::schema::find_crown;
 use temp_dir::TempDir;
 use std::fs::{File, rename};
-use std::cmp;
 use text_file_sort::sort::Sort;
 use crate::types::grain::Grain;
 use crate::types::line::Line;
-use crate::schema::{Leaves, Schema, Trunks};
+use crate::schema::Schema;
 use crate::select::select_schema;
 use crate::record::mow::mow;
 use async_stream::stream;
@@ -14,11 +13,9 @@ use futures_core::stream::{Stream, BoxStream};
 use futures_util::pin_mut;
 use futures_util::stream::StreamExt;
 use serde::{Deserialize, Serialize};
-use std::collections::HashMap;
 use std::path::PathBuf;
 use std::fs::OpenOptions;
 use std::fs;
-use std::io::Write;
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 struct Tablet {
@@ -57,14 +54,6 @@ fn plan_insert(schema: Schema, query: Entry) -> Vec<Tablet> {
     tablets
 }
 
-fn passthrough_tablet <S: Stream<Item = Entry>>(input: S) -> impl Stream<Item = Entry> {
-    stream! {
-        for await entry in input {
-            yield entry
-        }
-    }
-}
-
 fn insert_tablet<S: Stream<Item = Entry>>(input: S, path: PathBuf, tablet: Tablet) -> impl Stream<Item = Entry> {
     let filepath = path.join(&tablet.filename);
 
@@ -97,7 +86,7 @@ fn insert_tablet<S: Stream<Item = Entry>>(input: S, path: PathBuf, tablet: Table
             }).collect();
 
             for line in lines.iter() {
-                wtr.serialize(line);
+                wtr.serialize(line).unwrap();
             }
         }
     }
@@ -112,84 +101,18 @@ async fn insert_record_stream<S: Stream<Item = Entry>>(input: S, path: PathBuf) 
         for await query in input {
             strategy = plan_insert(schema.clone(), query.clone());
 
-            // let streams: Vec<&mut dyn Stream<Item = Entry>> = strategy.iter().map(|tablet| insert_tablet(tablet)).collect();
+            let query_stream = stream! {
+                yield query;
+            };
 
-            // let query_stream = stream! {
-            //     yield query;
-            // };
+            let mut stream: BoxStream<'static, Entry> = Box::pin(query_stream);
 
-            // boilerplate
-            // let insert_stream = insert_tablet(query_stream, path.clone(), strategy[0].clone());
-            // let insert_stream_1 = insert_tablet(insert_stream, path.clone(), strategy[1].clone());
+            for tablet in strategy.clone() {
+                stream = Box::pin(insert_tablet(stream, path.clone(), tablet));
+            }
 
-            // streams can't Vec
-            //let insert_streams = strategy.iter().fold(vec![], |with_stream, tablet| {
-            //    let insert_stream = if with_stream.len() == 0 {
-            //        insert_tablet(query_stream, path.clone(), tablet.clone())
-            //    } else {
-            //        insert_tablet(with_stream, path.clone(), tablet.clone())
-            //    };
-
-            //    vec![with_stream, vec![insert_stream]].concat()
-            //});
-
-            // query_stream and insert_tablet are different types
-            // let insert_streams = strategy.iter().fold(query_stream, |with_stream, tablet| {
-            //     insert_tablet(with_stream, path.clone(), tablet.clone())
-            // });
-
-            // query_stream and passthrough are different types
-            // let passthrough = |s| { stream! {
-            //     for await entry in input {
-            //         yield entry
-            //     }
-            // }};
-
-            // let insert_streams = strategy.iter().fold(passthrough(query_stream), |with_stream, tablet| {
-            //     insert_tablet(with_stream, path.clone(), tablet.clone())
-            // });
-
-            // even two calls to insert_tablet are different types
-            match strategy.len() {
-                0 => { yield query; },
-                1 => {
-                    let query_stream = stream! {
-                        yield query;
-                    };
-
-                    let insert_stream = insert_tablet(query_stream, path.clone(), strategy[0].clone());
-
-                    for await entry in insert_stream {
-                        yield entry;
-                    }
-                },
-                _ => {
-                    let query_stream = stream! {
-                        yield query;
-                    };
-
-                    // https://users.rust-lang.org/t/working-with-streams-combine-a-vector-of-streams-into-a-single-stream-of-a-custom-type/79244/4
-                    let mut stream: BoxStream<'static, Entry> = Box::pin(query_stream);
-
-                    for tablet in strategy.clone() {
-                        let insert_stream = insert_tablet(stream, path.clone(), tablet);
-
-                        stream = Box::pin(insert_stream);
-                    }
-                    // let insert_stream_first = insert_tablet(stream, path.clone(), strategy[0].clone());
-
-                    // let insert_stream = insert_tablet(insert_stream_first, path.clone(), strategy[1].clone());
-
-                    // let mut stream: BoxStream<'static, Entry> = Box::pin(insert_stream);
-
-                    // for i in 2..strategy.len()-1 {
-                    //     let insert_stream = insert_tablet(BoxStream::into_inner(stream), path.clone(), strategy[i].clone());
-                    // }
-
-                    for await entry in stream {
-                        yield entry;
-                    }
-                }
+            for await entry in stream {
+                yield entry;
             }
         }
 
