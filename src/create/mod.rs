83bb0c34895b7082d259d490b82f5b5bb542918e
Anton Davydov <fetsorn@gmail.com>
2025-02-25 14:12:27 +0300

    feat: add commands to cli

---
+++ src/create/mod.rs
@@ -0,0 +1,8 @@
+use std::path::PathBuf;
+
+pub async fn create_dataset(path: PathBuf, name: &str) {
+    // check that path/name doesn't exist
+    // create directory name
+    // write .csvs.csv
+    // write schema
+}
--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,19 @@ mod select;
 mod test;
 mod types;
 mod update;
+mod create;
 use insert::insert_record;
+use select::select_record;
+use update::update_record;
+use delete::delete_record;
+use create::create_dataset;
 use std::env;
 
+/// A command-line utility for comma separated value store datasets
 #[derive(Parser)]
-#[command(version, about)]
+#[command(version, arg_required_else_help = true)]
 struct Cli {
+    /// Path to the dataset
     #[arg(short, long)]
     path: Option<String>,
 
@@ -23,9 +30,35 @@ struct Cli {
 
 #[derive(Subcommand)]
 enum Commands {
-    Insert {
+    /// Find entries that match query
+    Select {
+        /// A json string in query object notation
         #[arg(short, long)]
         query: String,
+    },
+    /// Delete entries that match query
+    Delete {
+        /// A json string in query object notation
+        #[arg(short, long)]
+        query: String,
+    },
+    /// Update an entry from query
+    Update {
+        /// A json string in query object notation
+        #[arg(short, long)]
+        query: String,
+    },
+    /// Add an entry from query
+    Insert {
+        /// A json string in query object notation
+        #[arg(short, long)]
+        query: String,
+    },
+    /// Create a new dataset
+    Create {
+        /// Name of the dataset directory
+        #[arg(short, long)]
+        name: String,
     },
 }
 
@@ -41,6 +74,39 @@ async fn main() {
     // println!("Hello {}!", path.display());
 
     match &cli.command {
+        Some(Commands::Select { query }) => {
+            let query_json: serde_json::Value = serde_json::from_str(query).unwrap();
+
+            let query_record: types::entry::Entry = query_json.try_into().unwrap();
+
+            let entries = select_record(path, vec![query_record]).await;
+
+            for entry in entries.iter() {
+                println!("{}", entry);
+            }
+        },
+        Some(Commands::Delete { query }) => {
+            let query_json: serde_json::Value = serde_json::from_str(query).unwrap();
+
+            let query_record: types::entry::Entry = query_json.try_into().unwrap();
+
+            let entries = delete_record(path, vec![query_record]).await;
+
+            for entry in entries.iter() {
+                // println!("{}", entry);
+            }
+        },
+        Some(Commands::Update { query }) => {
+            let query_json: serde_json::Value = serde_json::from_str(query).unwrap();
+
+            let query_record: types::entry::Entry = query_json.try_into().unwrap();
+
+            let entries = update_record(path, vec![query_record]).await;
+
+            for entry in entries.iter() {
+                // println!("{}", entry);
+            }
+        },
         Some(Commands::Insert { query }) => {
             let query_json: serde_json::Value = serde_json::from_str(query).unwrap();
 
@@ -49,9 +115,14 @@ async fn main() {
             let entries = insert_record(path, vec![query_record]).await;
 
             for entry in entries.iter() {
-                // println!("Hello {}!", entry);
+                // println!("{}", entry);
             }
-        }
-        None => (),
+        },
+        Some(Commands::Create { name }) => {
+            create_dataset(path, name);
+        },
+        None => {
+            // show help
+        },
     }
 }
